d0ca84407d107b2bd15ecd5bfd2081b01920d2a5
Jonathan Villanueva <jonathanvillanueva171@gmail.com>
2026-08-12 15:44:16 -0700

    phase 1 decorators, api routes, error handling

--- app/main.py
+++ app/main.py
@@ -1,16 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+#how incoming json request body should be interpreted
+class JobApplication(BaseModel):
+    id: int 
+    company: str 
+    position: str 
+    status: str 
+
+class ApplicationStorage:   
+    applications: list[JobApplication] = []
 
 app = FastAPI(
     title="Jobflow API",
     version="0.1.0",
 )
 
-
 @app.get("/")
 def root() -> dict[str, str]:
     return {"message": "Jobflow API is running, Image update"}
 
+@app.post("/applications")
+def create_application(application: JobApplication):
+    #check if application already exist
+    applicant_id = application.id
+    for app in ApplicationStorage.applications:
+        if applicant_id == app.id:
+            raise HTTPException(
+                status_code=409,
+                detail="Application ID already"
+            )
+    ApplicationStorage.applications.append(application)
+    return application
 
-@app.get("/health")
-def health_check() -> dict[str, str]:
-    return {"status": "healthy"}
+@app.get("/applications")
+def get_applications():
+    return ApplicationStorage.applications
+
+@app.get("/applications/{application_id}")
+def get_application(application_id: int):
+    for application in ApplicationStorage.applications:
+        if application_id == application.id:
+            return application
+    
+    raise HTTPException(status_code=404, detail="Application not found")
+   

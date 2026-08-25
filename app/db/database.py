18bd21263e85a40907f4439a9f192d236efe1338
Jonathan Villanueva <jonathanvillanueva171@gmail.com>
2026-08-25 16:11:08 -0700

    add application database model

--- app/db/database.py
+++ app/db/database.py
@@ -1,27 +1,28 @@
 import asyncio
 import os
 
-from sqlalchemy import text, String, select
+from sqlalchemy import String, select, Enum
 from sqlalchemy.ext.asyncio import (
     create_async_engine, 
     async_sessionmaker
     )
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.exc import OperationalError 
+from sqlalchemy.exc import OperationalError
+from app.models.application import ApplicationStatus
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL)
 
 
 #Test engine connection
-async def test_connection():
-    try:
-        async with engine.connect() as connection:
-            result = await connection.execute(text("SElECT 1"))
-            print(result.scalar())
-        print("Connection Successfull")
-    except OperationalError as e:
-        print(f"Connection Failed: {e}")
+# async def test_connection():
+#     try:
+#         async with engine.connect() as connection:
+#             result = await connection.execute(text("SElECT 1"))
+#             print(result.scalar())
+#         print("Connection Successfull")
+#     except OperationalError as e:
+#         print(f"Connection Failed: {e}")
 
 
 #Session object maker
@@ -37,33 +38,34 @@ async_session_local = async_sessionmaker(
 class Base(DeclarativeBase):
     pass
 
-class Test(Base):
-    __tablename__ = "test"
+class Applications(Base):
+    __tablename__ = "application"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
+    company: Mapped[str] = mapped_column(String(255), nullable=False)
+    position: Mapped[str] = mapped_column(String(255), nullable=False)
+    status: Mapped[ApplicationStatus] = mapped_column(Enum(ApplicationStatus), nullable=False)
 
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    print("Table created")
+# async def create_tables():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+#     print("Table created")
 
 
 #verify if sessions are made
 async def test_session():
     async with async_session_local() as session:
         async with session.begin():
-            first_test = Test(name = "Test 1")
+            first_test = Applications(company = "Google", position = "Software Engineer", status = ApplicationStatus.OFFER)
             session.add(first_test)
 
-        result = await session.execute(select(Test))
+        result = await session.execute(select(Applications))
         test_result = result.scalars().all()
         for t in test_result:
-            print(t.name)
+            print(t.company, t.position, t.status)
 
 
 async def main():
-    await test_connection()
-    await create_tables()
+    # await create_tables()
     await test_session()
 asyncio.run(main())

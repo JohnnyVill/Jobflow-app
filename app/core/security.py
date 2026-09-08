2308dccd9d8ecb6c382cf55a203a31f993798609
Jonathan Villanueva <jonathanvillanueva171@gmail.com>
2026-09-07 18:37:31 -0700

    finalizing token setup

--- app/core/security.py
+++ app/core/security.py
@@ -1,20 +1,26 @@
 import jwt
 import os
 from app.models.users import UserResponse
+from datetime import datetime, timedelta, timezone
 
 
 
 key = os.getenv("JWT_SECRET_KEY")
+token_algorithm = os.getenv("JWT_ALGORITHM")
+expires_in = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",30))
 
 def create_access_token(user: UserResponse):
+    expiration = datetime.now(timezone.utc) + timedelta(minutes=expires_in)
+
     data = {
         "sub":str(user.id),
+        "exp": expiration
     }
 
     token = jwt.encode(
         payload=data,
         key=key,
-        algorithm="HS256"
+        algorithm=token_algorithm
     )
 
     return token
